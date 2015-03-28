fe7e6f4ebcacf1055572281694cc864bb7f4960a
Alex <tiparega@gmail.com>
2015-03-28 19:09:07 +0100

    Bootstrap metido para el nombre

--- web/censo/index.php
+++ web/censo/index.php
@@ -20,100 +20,69 @@ if ($POST["identificacion"]=="padron") {
 		<title>Nuevo simpatizante Ganar Alcorcón</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
 		<link rel="stylesheet" type="text/css" href="../css/estilo.css">
 		
 		<script type="text/javascript" src="../js/ext/jQuery/jquery.min.js" ></script>
 		<script type="text/javascript" src="../js/ext/bootstrap.min.js" ></script>
 		<script type="text/javascript" src="../js/censoWeb.js" ></script>
 	</head>
-	<body class="container">
-		<div class="container">
-			<form action="" accept-charset="utf-8" method="post" enctype="multipart/form-data" >
-				<div class="row">
-					<div class="col-md-2">
-						<label for="nombre">Nombre: </label>
-					</div>
-					<div class="col-md-10">
-						<input id="nombre" name="nombre" type="text" maxlength="50" autocomplete="off" required="required" />
-					</div>
+	<body class="container" onload="inicio()">
+		<form action="" accept-charset="utf-8" method="post" enctype="multipart/form-data" role="form" class="form-horizontal" >
+			<div class="form-group">
+				<label for="nombre" class="control-label col-sm-2">Nombre: </label>
+				<div class="col-sm-10">
+					<input id="nombre" name="nombre" type="text" class="form-control" size="50" maxlength="50" autocomplete="off" required="required" />
 				</div>
-				<div class="row">
-					<div class="col-md-2">
-						<label for="apellido1">Primer apellido: </label>
-					</div>
-					<div class="col-md-10">
-						<input id="apellido1" name="apellido1" type="text" maxlength="50" autocomplete="off" required="required" />
-					</div>
+			</div>
+			<div class="form-group">
+				<label for="apellido1">Primer apellido: </label> <input id="apellido1" name="apellido1" type="text" size="50" maxlength="50" autocomplete="off" required="required" />
+			</div>
+			<div class="form-group">
+				<label for="apellido2">Segundo apellido: </label> <input id="apellido2" name="apellido2" type="text" size="50" maxlength="50" autocomplete="off" required="required" />
+			</div>
+			<div class="form-group">
+				<label for="nif">NIF: </label> <input id="nif" name="nif" type="text" size="10" maxlength="10" autocomplete="off" required="required" />
+			</div>
+			<div class="form-group">
+				<label for="fechaNacimiento">Fecha de nacimiento: </label> <input id="fechaNacimiento" name="fechaNacimiento" type="text" size="50" maxlength="50" autocomplete="off" required="required" />
+			</div>
+			<div class="form-group">
+				<label for="email">Email: </label> <input id="email" name="email" type="email" size="100" maxlength="100" autocomplete="off" required="required" />
+			</div>
+			<div class="form-group">
+				<label for="telefono">Teléfono: </label> <input id="telefono" name="telefono" type="text" size="50" maxlength="50" autocomplete="off" required="required" />
+			</div>
+			<div class="form-group">
+				<label for="identificacion">Tipo de identificación</label>
+				<select id="identificacion" name="identificacion" onchange="cambiaId(this.value)">
+					<option value="">Seleccione un tipo</option>
+					<option value="dni">DNI</option>
+					<option value="nie">NIE</option>
+					<option value="padron">Padrón</option>
+				</select>
+			</div>
+			<p>Las imágenes deben enviarse en formatos jpg, png o gif. Puede ser un escaneado o una foto. Debe ser a color.</p>
+			<p>Los datos deben coincidir exactamente con los indicados en los documentos, en otro caso el registro será rechazado</p>
+			<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $CONFIG["MAX_IMG_SIZE"]?>" />
+			<div id="id_nif">
+				<div class="form-group">
+					<label for="nif_anv">Anverso: </label> <input id="nif_anv" name="nif_anv" type="file" />
 				</div>
-				<div class="row">
-					<div class="col-md-2">
-						<label for="apellido2">Segundo apellido: </label>
-					</div>
-					<div class="col-md-10">
-						<input id="apellido2" name="apellido2" type="text" maxlength="50" autocomplete="off" />
-					</div>
+				<div class="form-group">
+					<label for="nif_rev">Reverso: </label> <input id="nif_rev" name="nif_rev" type="file" />
 				</div>
-				<div class="row">
-					<div class="col-md-2">
-						<label for="nif">NIF: </label>
-					</div>
-					<div class="col-md-4">
-						<input id="nif" name="nif" type="text" maxlength="10" autocomplete="off" />
-					</div>
-					<div class="col-md-2">
-						<label for="fechaNacimiento">Fecha de nacimiento: </label>
-					</div>
-					<div class="col-md-4">
-						<input id="fechaNacimiento" name="fechaNacimiento" type="text" maxlength="10" autocomplete="off" required="required" />
-					</div>
+			</div>
+			<div id="id_padron">
+				<div class="form-group">
+					<label for="nif_anv">Padrón: </label> <input id="nif_anv" name="nif_anv" type="file" />
 				</div>
-				<div class="row">
-					<div class="col-md-2">
-						<label for="email">Email: </label>
-					</div>
-					<div class="col-md-10">
-						<input id="email" name="email" type="email" maxlength="100" autocomplete="off" required="required" />
-					</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<input type="submit" value="Enviar" />
 				</div>
-				<div class="row">
-					<div class="col-md-2">
-						<label for="telefono">Teléfono: </label>
-					</div>
-					<div class="col-md-10">
-						<input id="telefono" name="telefono" type="text" maxlength="9" autocomplete="off" required="required" />
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-2">
-						<label for="identificacion">Tipo de identifiación</label>
-					</div>
-					<div class="col-md-10">
-						<select id="identificacion" name="identificacion" onchange="cambiaId(this.value)">
-							<option value="dni">DNI</option>
-							<option value="nie">NIE</option>
-							<option value="padron">Padrón</option>
-						</select>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<p>Las imágenes deben enviarse en formatos jpg, png o gif. Puede ser un escaneado o una foto. Debe ser a color.</p>
-						<p>Los datos deben coincidir exactamente con los indicados en los documentos, en otro caso el registro será rechazado</p>
-					</div>
-				</div>
-				<div class="row">
-					<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $CONFIG["MAX_IMG_SIZE"]?>" />
-					<div id="id_nif" style="display:none">
-						<label for="nif_anv">Anverso: </label> <input id="nif_anv" name="nif_anv" type="file" />
-						<label for="nif_rev">Reverso: </label> <input id="nif_rev" name="nif_rev" type="file" />
-					</div>
-					<div id="id_padron"style="display:none">
-						<label for="nif_anv">Padrón: </label> <input id="nif_anv" name="nif_anv" type="file" />
-					</div>
-				</div>
-				
-				<input type="submit" value="Enviar" />
-			</form>
-		</div>
+			</div>
+		</form>
 	</body>
 </html>
--- web/js/censoWeb.js
+++ web/js/censoWeb.js
@@ -3,8 +3,15 @@ function cambiaId(metodo) {
 	if (metodo=="dni" || metodo=="nie") {
 		jQuery("#id_nif").show();
 		jQuery("#id_padron").hide();
-	} else if (metodo="padron") {
+	} else if (metodo=="padron") {
 		jQuery("#id_nif").hide();
 		jQuery("#id_padron").show();
+	} else {
+		jQuery("#id_nif").hide();
+		jQuery("#id_padron").hide();
 	}
+}
+
+function inicio() {
+	cambiaId(jQuery("#identificacion").val());
 }
